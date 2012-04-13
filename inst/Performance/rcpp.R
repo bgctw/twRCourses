a1e6996d9845fcad6881a318db551bc658a927a6
twutz <twutz@3ce85f96-4840-48e1-9cd0-f25c6e26bdd5>
2012-04-13 09:57:54 +0000

    added Rcpp package support

--- inst/Performance/rcpp.R
+++ inst/Performance/rcpp.R
@@ -18,3 +18,48 @@ cppf <- cxxfunction(signature(ns="integer", xs="numeric")
 
 benchmark( f(1e4,1), lf(1e4,1), cppf(1e4,1) )	# speedup of about 30
 	
+# may write C code of the function body to a file for fast prototyping
+fx <- cxxfunction( signature(),paste(readLines( "src/rcpp_hello_world_inline.cpp"), collapse = "\n" ), plugin = "Rcpp" )
+fx()
+
+.tmp.doesNotWorkBecauseOfIncludesAndFunctionDef <- function(){
+	inDir <- function(path, code) {
+		cur_dir <- getwd()
+		tryCatch({
+				setwd(path)
+				force(code)
+			}, finally = setwd(cur_dir))
+	}
+	fx <- inDir("src",{ 
+				tmp <- readLines( "rcpp_hello_world.cpp")
+				cxxfunction( signature(),paste( tmp[ !(1:length(tmp) %in% grep("#include",tmp)) ], collapse = "\n" ),
+				plugin = "Rcpp" )}
+	)
+}
+
+#-------- RCpp package
+#Rcpp.package.skeleton("cppTemplatePackage")
+pkg <- "twRCourses"
+# package needs to link to the Rcpp library (dll or so)
+# In file src/Makevars:	PKG_LIBS = `$(R_HOME)/bin/Rscript -e "Rcpp:::LdFlags()"`
+# In file src/Makevars.win:  PKG_LIBS = $(shell "${R_HOME}/bin${R_ARCH_BIN}/Rscript.exe" -e "Rcpp:::LdFlags()")
+# In file Namespace:  useDynLib(mypackage)
+
+
+.tmp.compile <- function(){
+	dynFilenameLocal <- file.path("src",paste(pkg, .Platform$dynlib.ext, sep = ""))
+	dyn.unload(dynFilenameLocal)
+	fnames <- paste(Sys.glob("src/*.cpp"), collapse=" ")
+	system(tmp <- paste("R CMD SHLIB -o ",dynFilenameLocal," ",fnames,sep=""))
+	#unlink(file.path("src","*.o"))
+	dyn.load(dynFilenameLocal)
+}
+(tmp <- .Call( "rcpp_hello_world", PACKAGE = pkg ))
+
+
+.tmp.testFromOtherWorkspace <- function(){
+	# R CMD INSTALL twRCourses
+	library(twRCourses)
+	pkg <- "twRCourses"
+	(tmp <- .Call( "rcpp_hello_world", PACKAGE = pkg ))
+}
---
+++ src/rcpp_hello_world_inline.cpp
@@ -0,0 +1,7 @@
+    using namespace Rcpp ;
+    
+    CharacterVector x = CharacterVector::create( "foo", "bar" )  ;
+    NumericVector y   = NumericVector::create( 0.0, 1.0 ) ;
+    List z            = List::create( x, y ) ;
+    
+    return z ;
