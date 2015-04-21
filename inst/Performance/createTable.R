87774d9b439e887b5cb99faf9df996bd1f675da0
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2015-04-21 15:06:14 +0200

    simplified performance optimization example (removed with)

--- inst/Performance/createTable.R
+++ inst/Performance/createTable.R
@@ -1,20 +1,18 @@
 
 createTable1 <- function( 
-    ### combine each month with each year and output to a matrix
-    parList=list(months=c("Jan","April","Dec"), years=1995:2010)   ##list with names of months and a list of years 
+        ### combine each month with each year and output to a matrix
+        parList=list(months=c("Jan","April","Dec"), years=1995:2010)   ##list with names of months and a list of years 
 ){
-    with( parList, {
-                res <- character(0)
-                for( month in months ){
-                    resY <- character(0)
-                    for( year in years ){
-                        resMY <- paste(month,year)
-                        resY <- c(resY, resMY )
-                    }
-                    res <- cbind( res, resY)
-                }
-                res
-            })
+    res <- character(0)
+    for( month in parList$months ){
+        resY <- character(0)
+        for( year in parList$years ){
+            resMY <- paste(month,year)
+            resY <- c(resY, resMY )
+        }
+        res <- cbind( res, resY)
+    }
+    res
 }
 
 createTable1()
