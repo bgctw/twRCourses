82bb9d43659192c39c4673c5da362da585eb6ab6
twutz <twutz@SADA.bgc.mpg.de>
2017-05-24 08:29:02 +0200

    parallel in more detail

--- inst/parallel/parallel1.R
+++ inst/parallel/parallel1.R
@@ -1,34 +1,46 @@
 library(parallel)
-
-#?parallel
-
-cl <- makeCluster(2)
-
-mySubFunction <- function(){ 
-  # called from myFancyFunction
-}
-clusterExport(cl, c("mySubFunction"))
+#?parallel     # see also vignette
+cl <- makeCluster( 2L )     # detectCores()
+#setDefaultCluster(NULL)
 
 
 myFancyFunction <- function(x){ 
+    Sys.sleep(0.1)  # wait for 1/10 of a second
+    1/x 
+}
+
+myData <- 1:50
+
+t1 <- Sys.time()
+result <- lapply(myData, myFancyFunction)
+(tSpent <- Sys.time() - t1)     # 5 seconds
+
+t1 <- Sys.time()
+resultPar <- parLapply( cl, myData, myFancyFunction)
+(tSpent2 <- Sys.time() - t1)    # 2.5 seconds    
+
+stopCluster(cl)
+
+#resultPar <- parLapplySave( cl, myData, myFancyFunction)
+
+
+#------------------- Need of exporting objects
+mySubFunction <- function(){}
+
+myMasterFunction <- function(x){ 
   Sys.sleep(0.1)  # wait for 1/10 of a second
   mySubFunction()
   1/x 
 }
-myFancyFunction(1)
 
-myData <- 1:50
+# produces error: "mySubFunction" not found
+parLapply(cl, 1:2, myMasterFunction )   
 
-
-t1 <- Sys.time()
-result <- lapply(myData, myFancyFunction)
-(tSpent <- Sys.time() - t1)
-
-t1 <- Sys.time()
-resultPar <- parLapply( cl, myData, myFancyFunction)
-(tSpent2 <- Sys.time() - t1)
-
-head(resultPar)
+# Need to explicitely transfer mySubFunction.
+# The envir argument is necessary if exporting from a function
+# instead of the global environment. 
+clusterExport(cl, c("mySubFunction"), envir=environment())
+parLapply(cl, 1:2, myMasterFunction )   
 
 
 
@@ -36,6 +48,8 @@ head(resultPar)
 throwsError <- function(x){ 
   stop("error in function throwsError.")
 }
+#resultPar <- parLapply(cl, 1:2, throwsError)
+
 # function generator dumpOnError in package twMisc
 #library(twMisc)
 dumpedFun <- dumpOnError(throwsError)  
